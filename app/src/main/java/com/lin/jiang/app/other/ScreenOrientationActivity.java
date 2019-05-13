d8e1144b0632cbb36321720173d5bdc1a032c2ea
Jiang Zhuoyan <jiangzhuoyan1984@gmail.com>
2019-05-13 10:57:30 +0800

    Test android activity orientation. Configuration.orientation VS activity.getRequestedOrientation().
    Change-Id: I53bd769ba39bfa87c9cdf28c484e444c68597df8

---
+++ app/src/main/java/com/lin/jiang/app/other/ScreenOrientationActivity.java
@@ -0,0 +1,14 @@
+package com.lin.jiang.app.other;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenOrientationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_orientation);
+    }
+}
