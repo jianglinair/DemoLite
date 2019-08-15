83e7b22f8a8ff1b67685a3292a0669bc4aa80c57
Jiang Zhuoyan <jiangzhuoyan1984@gmail.com>
2019-08-15 18:10:17 +0800

    更新部分demo
    Change-Id: Id869c7d57f95225da71c5ec08d546f75389617c9

--- app/src/main/java/com/lin/jiang/app/scroll/ScrollerLayout.java
+++ app/src/main/java/com/lin/jiang/app/scroll/ScrollerLayout.java
@@ -2,7 +2,6 @@ package com.lin.jiang.app.scroll;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -28,7 +27,7 @@ public class ScrollerLayout extends ViewGroup {
 
     private Scroller mScroller;
     private int mTouchSlop; // 最小滑动距离
-    private float mXDwon; // 手指按下时的坐标
+    private float mXDown; // 手指按下时的坐标
     private float mXMove; // 手指滑动时的实时坐标
     private float mXLastMove; // 上次触发 ACTION_MOVE 时的坐标
     private int leftBorder; // 界面可滚动的左边界
@@ -96,12 +95,12 @@ public class ScrollerLayout extends ViewGroup {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mXDwon = ev.getRawX();
-                mXLastMove = mXDwon;
+                mXDown = ev.getRawX();
+                mXLastMove = mXDown;
                 break;
             case MotionEvent.ACTION_MOVE:
                 mXMove = ev.getRawX();
-                float diff = Math.abs(mXMove - mXDwon);
+                float diff = Math.abs(mXMove - mXDown);
                 mXLastMove = mXMove;
                 // 手指滑动距离大于TouchSlop值时认为是滚动，此时拦截事件不分发给子控件，由自身的onTouchEvent()处理
                 if (diff > mTouchSlop) {
@@ -135,11 +134,25 @@ public class ScrollerLayout extends ViewGroup {
                 // 第二步，调用startScroll()方法来初始化滚动数据并刷新界面
                 mScroller.startScroll(getScrollX(), 0 , dx, 0);
                 invalidate();
+                performClick();
                 break;
             default:
         }
         return super.onTouchEvent(event);
     }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    //---------- 消除警告：overrides onTouchEvent but not performClick ----------//
+    /*=========================================================================
+     * 此警告实际上可以不用在意，因为一般不会在重写了 onTouchEvent() 之后还会给控件添加 onClick 事件。
+     * 消除办法：
+     * 1. 重写 performClick() 方法，直接 return super.performClick() 即可；
+     * 2. 在 ACTION_UP 分支中添加 performClick()，由于不会给此控件添加点击事件，所以 performClick() 实际上不会做实际的事情。
+     *========================================================================*/
 
     @Override
     public void computeScroll() {
@@ -149,4 +162,30 @@ public class ScrollerLayout extends ViewGroup {
             invalidate();
         }
     }
+
+    //---------- Scroller 代码模板 ----------//
+    /*
+    // 一、实例化Scroller对象，在自定义View中，mContext可以在自定义View的构造方法中获取
+    Scroller mScroller = new Scroller(mContext);
+
+    // 二、在一个自定义View中实现该方法，方法名可以自定义
+    public void smoothScrollTo(int destX,int destY){
+        int scrollX = getScrollX();
+        int scrollY  = getScrollY();
+        int dx = destX - scrollX;
+        int dy = destY - scrollY;
+        // 前两个参数表示起始位置，第三第四个参数表示位移量，最后一个参数表示时间
+        mScroller.startScroll(scrollX,scrollY,dx,dy,1000);
+        invalidate();
+    }
+
+    // 三、自定义View中重写该方法
+    @Override
+    public void computeScroll(){
+        if(mScroller.computeScrollOffset()){
+            scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            invalidate();
+        }
+    }
+    */
 }
